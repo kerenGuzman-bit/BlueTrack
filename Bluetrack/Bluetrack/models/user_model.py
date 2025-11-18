9165214850edb6d36cf52f10e5f94021a97ef0bb
kerenGuzman-bit <kguzmany@ucentral.edu.co>
2025-11-17 19:51:11 -0500

    Delete Bluetrack directory

--- Bluetrack/Bluetrack/models/user_model.py
+++
@@ -1,9 +0,0 @@
-from database import db
-
-# Definición del modelo de usuario
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    correo = db.Column(db.String(100), unique=True, nullable=False)  # Agregando correo
-    nombre = db.Column(db.String(100), nullable=False)  # Agregando nombre
-    login = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
